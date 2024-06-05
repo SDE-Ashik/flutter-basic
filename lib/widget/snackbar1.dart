d4aff45897c808bbd125772e8a04a03bd462d4f2
ashik <ashiashik5047@gmail.com>
2024-06-06 01:24:22 +0530

    snackbar widget

--- lib/widget/snackbar1.dart
+++ lib/widget/snackbar1.dart
@@ -6,6 +6,8 @@ class StckbarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 80, 99, 173),
       appBar: AppBar(
@@ -14,7 +16,7 @@ class StckbarWidget extends StatelessWidget {
             Navigator.push(
                 context, MaterialPageRoute(builder: (context) => HomePage()));
           },
-          icon: Icon(Icons.add_business),
+          icon: Icon(Icons.exit_to_app),
         ),
         title: Text(
           "hello",
@@ -28,25 +30,39 @@ class StckbarWidget extends StatelessWidget {
         backgroundColor: Colors.blue,
       ),
       body: Container(
-        child: ElevatedButton(
-          onPressed: () {
-            final snackbar = SnackBar(backgroundColor: Colors.red,
-            duration: Duration(milliseconds: 5),
+        
+        height: 300,
+        width: 300,
+        child: Center(
+          child: ElevatedButton(
             
-
-
-
-            content: Text("something went to wrong"),);
-
-
-            ScaffoldMessenger.of(context).showSnackBar(snackbar);
-          },
-          child: Center(
-            child: Text("click here"),
+            onPressed: () {
+              final snackbar = SnackBar(
+                backgroundColor: Colors.red,
+                duration: Duration(milliseconds: 5000),
+                behavior: SnackBarBehavior.floating,
+                elevation: 4,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(43)),
+                action: SnackBarAction(
+                  label: "home",
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomePage()));
+                  },
+                  textColor: Colors.green,
+                ),
+                content: Text("something went to wrong"),
+              );
+          
+              ScaffoldMessenger.of(context).showSnackBar(snackbar);
+            },
+            child: Center(
+              child: Text("click here"),
+            ),
+            style: ButtonStyle(
+                overlayColor: WidgetStateProperty.all(Colors.amber),
+                backgroundColor: WidgetStateProperty.all(Colors.black)),
           ),
-          style: ButtonStyle(
-              overlayColor: WidgetStateProperty.all(Colors.amber),
-              backgroundColor: WidgetStateProperty.all(Colors.black)),
         ),
       ),
     );
